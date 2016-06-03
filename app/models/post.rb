051d73143cea014276389e6db0ee163ed4559102
straysheep3 <getloose.kage@gmail.com>
2016-06-03 21:13:11 +0900

    add acts_as_votes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  acts_as_votable
 
   has_attached_file :image, styles: { medium: "500x500>", small: "350x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts do
+    member do
+      # postsコントローラにupvoteアクションを追加
+      get "like", to: "posts#upvote"
+      # postsコントローラにdownvoteアクションを追加
+      get "dislike", to: "posts#downvote"
+    end
     resources :comments
   end
 
