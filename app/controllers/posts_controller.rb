e25d9188a638f3aaa87d2f62086f43d74c35749d
straysheep3 <getloose.kage@gmail.com>
2016-06-03 18:55:27 +0900

    add User model name

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
-    #code
+    @posts = Post.all.order("created_at DESC")
   end
 
   def show
@@ -8,11 +11,16 @@ class PostsController < ApplicationController
   end
 
   def new
-    #code
+    @post = current_user.posts.build
   end
 
   def create
-    #code
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render "new"
+    end
   end
 
   def edit
@@ -20,10 +28,24 @@ class PostsController < ApplicationController
   end
 
   def update
-    #code
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render "edit"
+    end
   end
 
   def destroy
-    #code
+    @post.destroy
+    redirect_to root_path
   end
+
+  private
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :link, :description)
+    end
 end
